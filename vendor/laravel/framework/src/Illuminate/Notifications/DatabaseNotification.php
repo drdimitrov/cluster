645eb2f9ffa8e890113e258e1249769fd4db5ea1
Dragomir Dimitrov <info@nortiena.com>
2016-08-20 20:07:07 +0300

    gitignore changes

---
+++ vendor/laravel/framework/src/Illuminate/Notifications/DatabaseNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Illuminate\Notifications;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseNotification extends Model
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'notifications';
+
+    /**
+     * The guarded attributes on the model.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+        'read' => 'boolean',
+    ];
+
+    /**
+     * Get the notifiable entity that the notification belongs to.
+     */
+    public function notifiable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Mark the notification as read.
+     *
+     * @return void
+     */
+    public function markAsRead()
+    {
+        $this->forceFill(['read' => true])->save();
+    }
+
+    /**
+     * Create a new database notification collection instance.
+     *
+     * @param  array  $models
+     * @return \Illuminate\Notifications\DatabaseNotificationCollection
+     */
+    public function newCollection(array $models = [])
+    {
+        return new DatabaseNotificationCollection($models);
+    }
+}
